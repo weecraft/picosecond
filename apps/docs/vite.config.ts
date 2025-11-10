3433fdfc4ed5034d1431d679bdcca94927486212
Nyoman Sunima <nyomansunima@gmail.com>
2025-11-10 11:09:49 +0800

    feat: add initial docs

---
+++ apps/docs/vite.config.ts
@@ -0,0 +1,28 @@
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import tsconfigPaths from "vite-tsconfig-paths";
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsconfigPaths(),
+    tailwindcss(),
+    tanstackStart({
+      prerender: {
+        enabled: true,
+        autoStaticPathsDiscovery: true,
+        crawlLinks: true,
+      },
+      srcDirectory: "src/app",
+      sitemap: {
+        enabled: true,
+        host: "https://picosecond.vercel.app",
+      },
+    }),
+    react(),
+  ],
+});
